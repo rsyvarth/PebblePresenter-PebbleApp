753a71792821cbc95b1e991c57f9fe6258993c79
Robert Syvarth <robert@duxter.com>
2013-11-18 01:15:36 -0600

    HUGE font

--- src/pebblepresenter.c
+++ src/pebblepresenter.c
@@ -210,7 +210,7 @@ static void window_load(Window *window) {
   slide_timer_layer = text_layer_create((GRect) { .origin = { 5, 54 }, .size = bounds.size });
   text_layer_set_text_color(slide_timer_layer, GColorBlack);
   text_layer_set_background_color(slide_timer_layer, GColorClear);
-  text_layer_set_font(slide_timer_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+  text_layer_set_font(slide_timer_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_MEDIUM_NUMBERS));
 
   // status_layer = text_layer_create((GRect) { .origin = { 10, 54 + 14 }, .size = bounds.size });
   // text_layer_set_text_color(status_layer, GColorBlack);
